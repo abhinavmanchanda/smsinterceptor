91e7a8dd79ed7b09b8281545666c507e0414b579
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2013-06-08 15:29:00 +0530

    Revert "Final Commit."
    This reverts commit 1a434416b61ba993bd3b4d9a977cab0014f29dd9.

--- src/com/bootcamp/smsblocker/SmsReceiver.java
+++ src/com/bootcamp/smsblocker/SmsReceiver.java
@@ -5,25 +5,11 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.telephony.SmsMessage;
-import android.view.Gravity;
-import android.widget.Toast;
 
 public class SmsReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-//        Toast.makeText(context, "This is indeed an sms", Toast.LENGTH_SHORT).show();
-        System.out.println("++++++++++++++++++++++++++");
-        Bundle bundle = intent.getExtras();
-        Object messages[] = (Object[]) bundle.get("pdus");
-        SmsMessage smsMessage[] = new SmsMessage[messages.length];
-        for (int n = 0; n < messages.length; n++) {
-            smsMessage[n] = SmsMessage.createFromPdu((byte[]) messages[n]);
-        }
-        String incomingNumber = smsMessage[0].getOriginatingAddress();
-        Toast.makeText(context, "This is indeed a phone number - " + incomingNumber, Toast.LENGTH_SHORT).show();
-        System.out.println("++++++++++++++++"+ incomingNumber);
-        abortBroadcast();
     }
 
 
